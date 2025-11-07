3a01c9e15e23519dcd8148099dea9146cfcf44c8
liuyanghejerry <liuyanghejerry@126.com>
2025-11-08 00:41:22 +0800

    feat: prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,68 @@
 use crate::zed::LanguageServerId;
-use std::env;
+use std::fs;
+use zed::serde_json::json;
+use zed::serde_json::Value;
+use zed::Worktree;
 use zed_extension_api as zed;
 
-static OHOS_TYPESCRIPT_VERSION: &str = "4.9.5-r10";
+const LANGUAGE_SERVER_VERSION: &str = "latest";
+const LANGUAGE_SERVER_NAME: &str = "@arkts/language-server";
+const SERVER_PATH: &str = "node_modules/@arkts/language-server/bin/ets-language-server.mjs";
 
 struct MyArkTSExtension {
-    ohos_typescript_module_path: Option<String>,
+    language_server_path: Option<String>,
+}
+
+fn server_exists() -> bool {
+    fs::metadata(SERVER_PATH).map_or(false, |stat| stat.is_file())
+    // true
 }
 
 impl zed::Extension for MyArkTSExtension {
     fn new() -> Self {
-        let install_result = zed::npm_install_package("ohos-typescript", OHOS_TYPESCRIPT_VERSION);
-        let module_path = match install_result {
-            Ok(path) => {
-                println!("Successfully installed ohos-typescript at: {:?}", path);
-                Some(path)
-            }
-            Err(err) => {
-                println!("Failed to install ohos-typescript: {:?}", err);
-                None
-            }
-        };
-        println!("ohos-typescript path: {:?}", module_path);
+        if !server_exists() {
+            let install_result =
+                zed::npm_install_package(LANGUAGE_SERVER_NAME, LANGUAGE_SERVER_VERSION);
 
-        let node_binary = zed::node_binary_path().unwrap_or_else(|err| {
-            println!("Failed to get node binary path: {:?}", err);
-            std::process::exit(1);
-        });
-
-        println!("Node binary path: {:?}", node_binary);
-
-        let server_path = env::current_dir()
-            .unwrap()
-            .join("ohos-typescript/bin/tsserver")
-            .to_string_lossy()
-            .to_string();
-        Self {
-            ohos_typescript_module_path: Some(server_path),
+            match install_result {
+                Ok(_) => {
+                    println!("Successfully installed {}.", LANGUAGE_SERVER_NAME);
+                }
+                Err(err) => {
+                    println!("Failed to install {}: {:?}", LANGUAGE_SERVER_NAME, err);
+                    return Self {
+                        language_server_path: None,
+                    };
+                }
+            };
         }
+
+        Self {
+            language_server_path: Some(SERVER_PATH.to_string()),
+        }
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        _language_server_id: &LanguageServerId,
+        _worktree: &Worktree,
+    ) -> Result<Option<Value>, String> {
+        return Ok(Some(json!({
+            "typescript": {
+                "tsdk": "/Users/liuyanghejerry/develop/arkTS/ohos-typescript/lib"
+            },
+            "ohos": {
+                "resourceReferenceDiagnostic": "error",
+                // TODO: find them dynamically
+                "sdkPath": "DevEco-Studio-5.0.9.300.app/Contents/sdk/default/openharmony",
+                "etsComponentPath": "DevEco-Studio-5.0.9.300.app/Contents/sdk/default/openharmony/ets/component",
+                "etsLoaderPath": "DevEco-Studio-5.0.9.300.app/Contents/sdk/default/openharmony/ets/build-tools/ets-loader",
+                "lib": ["es6", "dom", "es2015"],
+                "typeRoots": ["DevEco-Studio-5.0.9.300.app/Contents/sdk/default/openharmony/types"],
+                "baseUrl": ".",
+                "paths": {}
+            }
+        })));
     }
 
     fn language_server_command(
@@ -50,23 +75,14 @@ impl zed::Extension for MyArkTSExtension {
             &zed::LanguageServerInstallationStatus::Downloading,
         );
 
-        let install_result = zed::npm_install_package("ohos-typescript", OHOS_TYPESCRIPT_VERSION);
-        let module_path = match install_result {
-            Ok(path) => {
-                println!("Successfully installed ohos-typescript at: {:?}", path);
-                Some(path)
-            }
-            Err(err) => {
-                println!("Failed to install ohos-typescript: {:?}", err);
-                None
-            }
-        };
-        println!("ohos-typescript path: {:?}", module_path);
+        if self.language_server_path.is_none() {
+            return Err("language-server installation failed.".to_string());
+        }
 
         Ok(zed::Command {
             command: zed::node_binary_path()?,
             args: vec![
-                self.ohos_typescript_module_path.clone().unwrap(),
+                self.language_server_path.clone().unwrap(),
                 "--server-mode".to_string(),
                 "--stdio".to_string(),
             ],
